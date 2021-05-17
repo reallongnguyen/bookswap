997f8c7cbaad20f7d757e08a6cb3bb98905b1af6
Long Nguyen Phuc <reallongnguyen@gmail.com>
2021-05-18 03:49:52 +0900

    edit: ui

--- src/page/tinder/index.tsx
+++ src/page/tinder/index.tsx
@@ -8,15 +8,27 @@ const Tinder: FC = () => {
         Discover
       </div>
       <div className='absolute top-24 w-full px-6'>
-        <div className='aspect-w-3 aspect-h-4 object-cover'>
-          <img
-            className='rounded-2xl shadow'
-            src='https://cdn2.tieudungplus.vn/media/uploaded/27/2017/07/17/IMG_0735.jpg'
-            alt=''
-          />
+        <div className='relative'>
+          <div className='aspect-w-3 aspect-h-4 object-cover'>
+            <img
+              className='rounded-xl shadow-md'
+              src='https://cdn2.tieudungplus.vn/media/uploaded/27/2017/07/17/IMG_0735.jpg'
+              alt=''
+            />
+          </div>
+          <div className='absolute w-full bottom-0 h-16 flex items-center bg-white rounded-xl'>
+            <div className='px-4'>
+              <div className='text-lg font-semibold'>
+                Sapiens - Lược sử về loài người
+              </div>
+              <div className='-mt-1 text-gray-400 text-sm font-light'>
+                Thanh Xuân, Hà Nội
+              </div>
+            </div>
+          </div>
         </div>
-        <div className='mt-4 text-xl font-semibold'>
-          Sapiens - Lược sử về loài người
+        <div className='mt-4 flex justify-around'>
+          <button className='outline-none active:outline-none focus:outline-none active:shadow-sm shadow-md border-gray-200 border rounded-full w-14 h-14'></button>
         </div>
       </div>
     </div>
